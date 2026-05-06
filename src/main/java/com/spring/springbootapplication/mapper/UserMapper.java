bd7d1cc4cb53532d4df784bd24aad35b31ef866d
tachiya kaoru <picky.0703@gmail.com>
2026-05-07 01:13:15 +0900

    feat: add registration page and user persistence foundation

---
+++ src/main/java/com/spring/springbootapplication/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.spring.springbootapplication.mapper;
+
+import com.spring.springbootapplication.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+
+    long countByEmail(@Param("email") String email);
+
+    int insert(User user);
+}
