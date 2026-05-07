a995ea68099d124cfbb3cfc6a8246084d1300d3b
tachiya kaoru <picky.0703@gmail.com>
2026-05-07 10:41:00 +0900

    chore: configure mybatis mapper scanning

--- src/main/java/com/spring/springbootapplication/SpringbootApplication.java
+++ src/main/java/com/spring/springbootapplication/SpringbootApplication.java
@@ -2,8 +2,10 @@ package com.spring.springbootapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.spring.springbootapplication.mapper")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
