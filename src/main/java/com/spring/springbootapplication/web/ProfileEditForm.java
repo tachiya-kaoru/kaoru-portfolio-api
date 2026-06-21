5d5f5f649cb1eba04bfa6d8ed655dfcfb436950d
tachiya kaoru <picky.0703@gmail.com>
2026-06-22 02:15:21 +0900

    fix(profile): preserve intro line breaks and split validation messages

--- src/main/java/com/spring/springbootapplication/web/ProfileEditForm.java
+++ src/main/java/com/spring/springbootapplication/web/ProfileEditForm.java
@@ -1,9 +1,10 @@
 package com.spring.springbootapplication.web;
 
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 
 public class ProfileEditForm {
-
+    @NotBlank(message = "自己紹介は必須項目です")
     @Size(min = 50, max = 200, message = "自己紹介は50文字以上200文字以下で入力してください")
     private String introduction;
 
